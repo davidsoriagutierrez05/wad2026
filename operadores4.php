448e38611f890de2d595ebec8c6009c38bbd5684
davidsoriagutierrez05 <davidsoriagutierrez05@gmail.com>
2026-05-15 19:09:35 +0200

    operadores4 php

---
+++ operadores4.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title></title>
+</head>
+
+<body>
+<?php
+
+	$num1 = 5;
+	$num2 = 10;
+    echo $num1;
+    echo "<br>";
+    echo $num1++;
+    echo "<br>";
+    echo $num1;
+    echo "<br>";echo "<br>";
+    echo $num2;
+    echo "<br>";
+    echo $num2;
+
+    ?>
+</body>
+</html>
