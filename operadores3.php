78b8717d729d75bd96c5dccb0b304f9b5836c3b5
davidsoriagutierrez05 <davidsoriagutierrez05@gmail.com>
2026-05-15 19:08:43 +0200

    operadores3 php

---
+++ operadores3.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Practicas PHP</title>
+</head>
+
+<body>
+<?php
+
+	$num1 = 5;
+	$num2 = 10;
+	$num3 = 5;
+	$valor1 = "5";
+
+    //operadores de comparacion
+    if($num1 == $num2){
+			echo "Iguales num1 y num2";
+    }
+
+    if($num1 !== $valor1){
+			echo "No idénticos num1 y valor1";
+    }
+    ?>
+</body>
+</html>
