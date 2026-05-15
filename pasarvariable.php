fe4558bf1b30184cafe58d485356acaed496fa46
davidsoriagutierrez05 <davidsoriagutierrez05@gmail.com>
2026-05-15 17:59:47 +0200

    pasarvariable php

---
+++ pasarvariable.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Prácticas PHP</title>
+</head>
+
+<body>
+    <a href="destino.php?nombre=David&cargo=Soria"> Pasar las variables a destino.php </a>
+    <?php
+
+    ?>
+</body>
+</html>
