646052857c3fcc6935078a27d2fa867b1a995784
davidsoriagutierrez05 <davidsoriagutierrez05@gmail.com>
2026-05-16 15:32:44 +0200

    dowhile php

---
+++ dowhile.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Prácticas PHP</title>
+</head>
+<body>
+    <?php 
+
+        $valor = 10;
+        
+        while($valor !=10){
+            echo "Dentro del while";
+            echo "<br>";
+        }
+
+        do{
+            echo "Dentro del do while";
+        }while($valor !=10);
+
+
+
+    ?>
+</body>
+</html>
