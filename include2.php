460ac8b013b280d75a9f18067cc4404b4fa8986f
davidsoriagutierrez05 <davidsoriagutierrez05@gmail.com>
2026-05-16 15:36:03 +0200

    include2 php

---
+++ include2.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Prácticas PHP</title>
+</head>
+<body>
+    <?php 
+
+        echo "Soy el segundo fichero";
+
+    ?>
+</body>
+</html>
