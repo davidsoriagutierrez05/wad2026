4dd2bfdc932f8f690819e1a5d56921b44fa84231
davidsoriagutierrez05 <davidsoriagutierrez05@gmail.com>
2026-05-19 17:09:14 +0200

    actualizar php

--- actualizar.php
+++ actualizar.php
@@ -1,38 +1,15 @@
-<!doctype html>
-<html>
-<head>
-<meta charset="utf-8">
-<title>Comprobar</title>
-</head>
+<?php
+require_once 'conexion.php';
+$id = $_GET['id'];
 
-<body>
-<?php 
+$sql = "UPDATE objetivos SET completo_objetivo = '1' WHERE id_objetivo = '" . $id . "'";
+if(mysqli_query($conexion, $sql)){
+  print ("Actualizado");
+} else {
+  print("Fallido");
+}
+
+echo "<script>location.href='index.php'</script>";
 
 
-	//capturo el valor del campo de formulario
-	$minombre = $_GET["nombre"];
-	
-	//capturo el valor del desplegable
-	$modificar = $_GET["seleccionar"];
-	
-	//Creo las variables de conexión a MySQL
-	$host = "localhost";
-	$usuario = "root";
-	$pass = "";
-	
-	//Establecer la conexión con MySQL
-	$conexion = mysqli_connect($host,$usuario,$pass) or die("Error de conexión");
-	
-	//Seleccionamos la Base de Datos
-	mysqli_select_db($conexion,"usuarios");
-	
-	//creamos la sentencia SQL de consulta
-	 $sql = "UPDATE clientes SET nombre = '$minombre' WHERE nombre = '$modificar'";
-	
-	//Ejecutar la sentencia SQL
-	mysqli_query($conexion,$sql);
-	
 ?>
-
-</body>
-</html>
